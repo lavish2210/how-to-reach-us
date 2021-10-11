92f0be741190ac1ea6e74f53f366164ac922c746
Lavish Sachdeva <b20113@students.iitmandi.ac.in>
2021-10-11 17:57:39 +0530

    Added basic template

---
+++ index.js
@@ -0,0 +1,22 @@
+var coll = document.getElementsByClassName("label");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function(){
+    var lastElement = this.lastChild;
+    var firstElement = this.firstChild.nextElementSibling;
+    if(lastElement.classList.contains("hidden"))
+    {
+      lastElement.classList.remove("hidden");
+      lastElement.classList.add("visible");
+      firstElement.style.transform = 'rotate(90deg)';
+    }
+    else
+    {
+      lastElement.classList.remove("visible");
+      lastElement.classList.add("hidden");
+      lastElement.style.fonSize = "100px";
+      firstElement.style.transform = 'rotate(0deg)';
+    }
+  });
+}
